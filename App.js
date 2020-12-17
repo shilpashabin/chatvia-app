cca4cb9958d74ecc028904e4a5f430ad70150c33
Rakhil PR <rakhilpr96@gmail.com>
2020-12-17 13:12:53 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+      <Text>ChatVia</Text>
+    </View>
+  );
+}
+
+export default App;
